4985020e2283bc6dd9aad620ede97dac3c68b187
sie.rra <rudiazbern@gmail.com>
2023-01-10 10:32:31 +0100

    bug: Update basePath from next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	basePath: '/prtfolio' // TODO: Change this when is other site
 }
 
 module.exports = nextConfig
