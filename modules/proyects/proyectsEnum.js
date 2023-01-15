eddf5587655ffe1958603a78a5fd804019408be4
sie.rra <rudiazbern@gmail.com>
2023-01-15 17:59:54 +0100

    feat: added projects

---
+++ modules/proyects/proyectsEnum.js
@@ -0,0 +1,76 @@
+export const proyectsUI_UX = [
+	{
+		title: 'Custom Lego',
+		date: 'Dic. 2022',
+		imageSrc: '/prtfolio/img/CustomLego.png',
+		duration: '15 días',
+		role: 'Product Designer',
+		typeOfWork: 'Individual',
+		mediumLink:
+			'https://medium.com/@nataliadelbarcorojas/custom-lego-5085af87ed9c',
+		figmaLink:
+			'https://www.figma.com/proto/nrpQra1vNgfVTagKErfVB7/Custom-Lego?page-id=208%3A2735&node-id=233%3A4323&viewport=715%2C130%2C0.11&scaling=scale-down&starting-point-node-id=233%3A4323',
+		content:
+			'Un proyecto para proporcionar una comunidad a los usuarios y diseñadores de Lego centrado en hacer accesible este hobbie a todos los bolsillos y valorar el trabajo de los diseñadores. Los fans de Lego necesitan encontrar construcciones alternativas originales creadas por otros usuarios pero tienen dificultades encontrando webs y apps que puedan proporcionarlos y vender las piezas necesarias para cada construcción. ¿Cómo podemos solucionar este pain point de los usuarios?'
+	},
+	{
+		title: 'Urbango',
+		date: 'Nov. 2022',
+		imageSrc: '/prtfolio/img/mockupUrbango.jpg',
+		duration: '15 días',
+		role: 'Product Designer',
+		typeOfWork: 'Individual',
+		mediumLink:
+			'https://medium.com/@nataliadelbarcorojas/urbango-2beddcf72814',
+		figmaLink:
+			'https://www.figma.com/proto/Puduvc9uz3qidnd5j9OSGb/Urbango?page-id=164%3A364&node-id=204%3A3599&viewport=103%2C559%2C0.1&scaling=scale-down&starting-point-node-id=204%3A3599',
+		content:
+			'Con la nueva ley que está diseñando el gobierno sobre los VMP (Vehículos Movilidad Personal), obligará a que este tipo de vehículos tenga un seguro en regla para poder circular. En unos meses los usuarios más jóvenes se verán en una encrucijada cuando la ley esté en vigor ¿Cómo podemos acercar el mundo de los seguros a estos usuarios?'
+	},
+	{
+		title: 'Feature Tinder',
+		date: 'Nov. 2022',
+		imageSrc: '/prtfolio/img/TinderFeature.png',
+		duration: '15 días',
+		role: 'Product Designer',
+		typeOfWork: 'Grupal',
+		mediumLink:
+			'https://medium.com/@nataliadelbarcorojas/feature-tinder-b5e52e2af709',
+		figmaLink:
+			'https://www.figma.com/proto/wG0mdLlScZPfX752c7m6t5/Tinder?page-id=64%3A499&node-id=230%3A6104&viewport=834%2C517%2C0.05&scaling=scale-down&starting-point-node-id=163%3A735&show-proto-sidebar=1',
+		content:
+			'Un proyecto de incrementar una nueva funcionalidad en Tinder, la app más popular para encontrar el amor en toda España. Las usuarias de la app de citas necesitan sentirse seguras para poder quedar con su cita sin tener ningún percance. ¿Cómo podemos ayudarlas a que se sientan tranquilas a la hora de quedar con su match?'
+	},
+	{
+		title: 'Comercial Pazos',
+		date: 'Oct. 2022',
+		imageSrc: '/prtfolio/img/ComercialPazos.png',
+		duration: '15 días',
+		role: 'Product Designer',
+		typeOfWork: 'Grupal',
+		content:
+			'Un proyecto de rediseño de un e-commerce dedicado a la venta de herramientas para profesionales de la artesanía en madera. Los usuarios necesitan encontrar sus herramientas profesionales para la madera pero tienen dificultad de navegar por la web de Comercial Pazos, ya que tiene más de una veintena de categorías. ¿Cómo podemos mejorar la experiencia del usuario comprando online?'
+	},
+	{
+		title: 'Eras+',
+		date: 'Oct. 2022',
+		imageSrc: '/prtfolio/img/Eras+.png',
+		duration: '5 días',
+		role: 'UX Designer',
+		typeOfWork: 'Grupal',
+		mediumLink:
+			'https://medium.com/@nataliadelbarcorojas/proyecto-1-ironhack-eras-d3df211e8c1b',
+		content:
+			'Un proyecto de diseño de una app para proporcionar los documentos necesarios a los universitarios que desean irse de Erasmus centrados en hacer sencilla e intuitiva toda la burocracia y que la experiencia del estudiante sea inolvidable. Migrar a un nuevo país conlleva una serie de retos, con ello vienen una serie de trámites burocráticos que deben ser completados en un límite de tiempo. ¿Cómo podríamos ayudar a las personas migrantes a encontrar la información y completar los trámites a tiempo?'
+	},
+	{
+		title: 'Vegrows',
+		date: 'Jun. 2022',
+		imageSrc: '/prtfolio/img/Vegrows.png',
+		duration: '3 meses',
+		role: 'Product Designer',
+		typeOfWork: 'Individual',
+		content:
+			'Nuestro objetivo es desarrollar una aplicación de recetas vegetarianas que atienda a las necesidades de los usuarios vegetarianos que buscan una amplia variedad de recetas vegetarianas saludables y fáciles de seguir. La aplicación debe tener una base de datos amplia y actualizada de recetas vegetarianas, con opciones para filtrar por ingredientes, nivel de dificultad, tiempo de preparación, etc. También debe ser fácil de usar y tener una interfaz intuitiva. Nuestro problema es cómo crear una aplicación que cumpla con las necesidades de nuestro público objetivo, sea fácil de usar y atractiva visualmente.'
+	}
+]
