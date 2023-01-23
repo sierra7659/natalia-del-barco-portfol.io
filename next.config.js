574dff1df5b9f8131e0cf0184210f45cbc38c9f8
Natalia del Barco <119662100+NataliaDelBarco@users.noreply.github.com>
2023-01-23 22:48:02 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	basePath: '/portfolio-nxtjs' // TODO: Change this when is other site
+	reactStrictMode: true // TODO: Change this when is other site
 }
 
 module.exports = nextConfig
